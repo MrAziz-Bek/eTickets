9df59b5285f223b76a12229917b98d5f8eb16a79
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-10-14 22:23:16 +0500

    Adding first model

---
+++ src/eTickets/Models/Actor.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models;
+
+public class Actor
+{
+    [Key]
+    public int Id { get; set; }
+
+    public string ProfilePictureURL { get; set; }
+    public string Fullname { get; set; }
+    public string Bio { get; set; }
+}
