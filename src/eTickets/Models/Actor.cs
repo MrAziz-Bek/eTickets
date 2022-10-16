35b9a1d61ef20714a7269ee505a978c49074ad23
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-10-16 19:24:45 +0500

    Adding ProducersController

--- src/eTickets/Models/Actor.cs
+++ src/eTickets/Models/Actor.cs
@@ -7,7 +7,7 @@ public class Actor
     [Key]
     public int Id { get; set; }
 
-    [Display(Name = "Profile Picture URL")]
+    [Display(Name = "Profile Picture")]
     public string ProfilePictureURL { get; set; }
 
     [Display(Name = "Full Name")]
