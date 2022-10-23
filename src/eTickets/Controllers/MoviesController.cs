a121f0e60d348f30e67a64afbd22f4f66ecbef74
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-10-23 09:50:32 +0500

    MoviesService configuration with EntityBaseRepository

--- src/eTickets/Controllers/MoviesController.cs
+++ src/eTickets/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using eTickets.Data;
+using eTickets.Data.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
@@ -12,16 +13,16 @@ namespace eTickets.Controllers;
 
 public class MoviesController : Controller
 {
-    private readonly AppDbContext _context;
+    private readonly IMoviesService _service;
 
-    public MoviesController(AppDbContext context)
+    public MoviesController(IMoviesService service)
     {
-        _context = context;
+        _service = service;
     }
 
     public async Task<IActionResult> Index()
     {
-        var allMovies = await _context.Movies.Include(m => m.Cinema).OrderBy(m => m.Name).ToListAsync();
+        var allMovies = await _service.GetAllAsync();
         return View(allMovies);
     }
 }
