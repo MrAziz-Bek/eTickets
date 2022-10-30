02ea00fca3bd3b42a109d716b2de1023665976b8
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-10-30 17:16:08 +0500

    Logging in users

--- src/eTickets/Controllers/AccountController.cs
+++ src/eTickets/Controllers/AccountController.cs
@@ -22,9 +22,37 @@ public class AccountController : Controller
     {
         _userManager = userManager;
         _signInManager = signInManager;
-        _context = context;   
+        _context = context;
     }
 
     public IActionResult Login()
         => View(new LoginViewModel());
+
+    [HttpPost]
+    public async Task<IActionResult> Login(LoginViewModel loginViewModel)
+    {
+        if (!ModelState.IsValid)
+            return View(loginViewModel);
+
+        var user = await _userManager.FindByEmailAsync(loginViewModel.EmailAddress);
+        if (user is not null)
+        {
+            var passwordCheck = await _userManager.CheckPasswordAsync(user, loginViewModel.Password);
+
+            if (passwordCheck)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, loginViewModel.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Movies");
+                }
+            }
+            TempData["Error"] = "Wrong credentials. Please, try again!";
+            return View(loginViewModel);
+        }
+
+        TempData["Error"] = "Wrong credentials. Please, try again!";
+        return View(loginViewModel);
+    }
 }
